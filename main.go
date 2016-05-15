703bcc47e86b1790a2d6c2cd45f791177bd00b73
Toomore Chiang <toomore0929@gmail.com>
2016-05-15 12:37:15 +0800

    Add `md5sum`.

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 )
+
+func md5sum(f *os.File) []byte {
+	hash := md5.New()
+	io.Copy(hash, f)
+	return hash.Sum(nil)
+}
 
 func walkfeed(path string, info os.FileInfo, err error) error {
 	if err == nil {
@@ -13,6 +21,11 @@ func walkfeed(path string, info os.FileInfo, err error) error {
 			fmt.Println("Folder: ", path)
 		} else {
 			fmt.Println(info.Mode().String(), info.IsDir(), path, info.Size())
+			f, err := os.Open(path)
+			defer f.Close()
+			if err == nil {
+				fmt.Printf("%x\n", md5sum(f))
+			}
 		}
 	} else {
 		log.Println(err)
@@ -21,6 +34,6 @@ func walkfeed(path string, info os.FileInfo, err error) error {
 }
 
 func main() {
-	err := filepath.Walk("/Volumes/RamDisk/", walkfeed)
+	err := filepath.Walk("./", walkfeed)
 	fmt.Println(err)
 }
