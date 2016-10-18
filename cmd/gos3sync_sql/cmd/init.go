5b7ddd801006d4d37aa462f92640734440cffae5
Toomore Chiang <toomore0929@gmail.com>
2016-10-18 22:32:27 +0800

    Try using Cobra.

---
+++ cmd/gos3sync_sql/cmd/init.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init [path]",
+	Short: "init a new sync folder",
+	Long:  `init a new sync folder, create db.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("init called", args, cmd.Flags().Lookup("name").Value)
+		if len(args) == 1 {
+			log.Println(cmd.CommandPath())
+			//syncdb.Init(args[0])
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	initCmd.Flags().BoolP("name", "n", false, "Need name")
+
+}
--- cmd/gos3sync_sql/main.go
+++ cmd/gos3sync_sql/main.go
@@ -1,46 +1,41 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
+import "github.com/toomore/gos3sync/cmd/gos3sync_sql/cmd"
 
-	"github.com/toomore/gos3sync"
-	"github.com/toomore/gos3sync/syncdb"
-)
+//func showHelp() {
+//	fmt.Println(`
+//usage: gos3sync_sql [command]
+//
+//command:
+//	init [path]
+//`)
+//}
 
-func showHelp() {
-	fmt.Println(`
-usage: gos3sync_sql [command]
-
-command:
-	init [path]
-`)
-}
+//func main() {
+//	flag.Parse()
+//	if len(flag.Args()) < 1 {
+//		showHelp()
+//		os.Exit(0)
+//	}
+//	switch flag.Arg(0) {
+//	case "init":
+//		syncdb.Init(flag.Arg(1))
+//	case "path":
+//		log.Println(filepath.Abs("./"))
+//	case "add":
+//		fw := &gos3sync.FileWalk{}
+//		fw.Walk(flag.Arg(1))
+//		log.Println(fw.Paths)
+//		log.Println("Do add file")
+//	case "commit":
+//		log.Println("Save to index")
+//	case "push":
+//		log.Println("Upload to AWS S3")
+//	default:
+//		showHelp()
+//	}
+//}
 
 func main() {
-	flag.Parse()
-	if len(flag.Args()) < 1 {
-		showHelp()
-		os.Exit(0)
-	}
-	switch flag.Arg(0) {
-	case "init":
-		syncdb.Init(flag.Arg(1))
-	case "path":
-		log.Println(filepath.Abs("./"))
-	case "add":
-		fw := &gos3sync.FileWalk{}
-		fw.Walk(flag.Arg(1))
-		log.Println(fw.Paths)
-		log.Println("Do add file")
-	case "commit":
-		log.Println("Save to index")
-	case "push":
-		log.Println("Upload to AWS S3")
-	default:
-		showHelp()
-	}
+	cmd.Execute()
 }
